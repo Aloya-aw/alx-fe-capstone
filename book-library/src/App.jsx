a7f3b17c13c09d22f4ba1605aa11f5556c735843
Anna-Katherine Aloya Awindor <aloyaawindor@live.com>
2024-12-09 00:04:13 +0000

    changes mad to readme and app.jsx

--- book-library/src/App.jsx
+++ book-library/src/App.jsx
@@ -24,9 +24,10 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <p>This is a test change</p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Anna and React logos.
       </p>
     </>
   )
