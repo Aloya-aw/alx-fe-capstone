16bda62e3e83ffbaa392bb25850f27dc394a3b7e
Anna-Katherine Aloya Awindor <aloyaawindor@gmail.com>
2024-12-23 10:11:41 +0000

    BookDetails.jsx component created. This component will be expanded later to display the fetched book details.

---
+++ book-library/src/components/BookDetails.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const BookDetails = () => {
+  return (
+    <div>
+      {/* Placeholder for book details */}
+      <h2>Book Details</h2>
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+export default BookDetails;
