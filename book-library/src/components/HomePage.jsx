132b031b7e1b595afc648698c45a110c707b1cc7
Anna-Katherine Aloya Awindor <aloyaawindor@gmail.com>
2025-01-04 19:06:09 +0000

    Logo imported and applied properly in HomePage.jsx

--- book-library/src/components/HomePage.jsx
+++ book-library/src/components/HomePage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import { useNavigate } from 'react-router-dom';
 import background from '../assets/background.png';
-import logo from '../assets/logo.png';
+import Logo from '../assets/Logo.png';
 
 const HomePage = () => {
     const navigate = useNavigate(); 
@@ -14,7 +14,7 @@ const HomePage = () => {
         <div>
             <div className="relative bg-cover bg-center bg-no-repeat" style={{ backgroundImage: `url(${background})` }}>
                 <div className="container mx-auto flex flex-col">
-                    <img src={logo} alt="Record Reads logo" className="mt-10 w-72 h-20 ml-11"/>
+                    <img src={Logo} alt="Record Reads logo" className="mt-10 w-72 h-20 ml-11"/>
                     <div className="w-full mx-auto flex flex-col items-center justify-center mb-28 mt-12">
                         <h1 className="mx-auto flex sm:w-full flex-col justify-center shrink-0 text-white text-center font-Imprint sm:text-[80px] font-normal leading-[normal] mt-3 text-[40px] w-80">Browse the largest collection of books on the internet!</h1>
                         <SearchBar onSearch={handleSearch} />
